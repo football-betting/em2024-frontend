fdc5058716a8bfc436efef69c3f85dd866693880
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-03 17:00:01 +0200

    style login page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Chakra Petch', 'sans-serif'],
+				italic: ['Chakra Petch', 'sans-serif']
+			},
+			fontSize: {
+				xs: ['0.6rem', '0.8rem'],
+				base: ['1rem', '1.3rem'],
+				heading: ['1.625rem', '1.95rem']
+			},
+			fontWeight: {
+				normal: '400'
+			}
+		}
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
+		function({ addBase }) {
+			addBase({
+				'h1, h2, h3, h4, h5, h6': { fontFamily: 'Bebas Neue, sans-serif', fontSize: '1.625rem', lineHeight: '1.95rem' },
+				'body': { fontFamily: 'Chakra Petch, sans-serif' },
+			});
+		}
 	],
 }
