ded2417e763fda930540f266743c2840d5bd534b
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-06 11:22:40 +0200

    add tips test

--- src/lib/tip.ts
+++ src/lib/tip.ts
@@ -1,21 +1,40 @@
 import db from "../core/db.ts";
-import {eq} from "drizzle-orm";
+import {and, eq} from "drizzle-orm";
 import {tip} from "../../db/schemas/schema.ts";
 import type {Tip} from "../interfaces/tip.ts";
 
 
+export async function saveTip(userId: number, matchId: number, scoreHome: number, scoreAway: number) {
+    const tipDb = await getTipByUserAndMatch(userId, matchId);
+    if (tipDb) {
+        tipDb.scoreHome = scoreHome;
+        tipDb.scoreAway = scoreAway;
 
-export async function createUser(newTip: Tip) {
+        tipDb.date = new Date();
+        await db.update(tip).set(tipDb).where(eq(tip.id, tipDb.id));
+        return;
+    }
+
     await db.insert(tip).values({
-        userId: newTip.userId,
-        matchId: newTip.matchId,
-        date: newTip.date,
-        scoreHome: newTip.scoreHome,
-        scoreAway: newTip.scoreAway
+            userId: userId,
+            matchId: matchId,
+            date: new Date(),
+            scoreHome: scoreHome,
+            scoreAway: scoreAway
     });
 }
 
-export async function updateUser(newTip: Tip) {
-    await db.update(tip).set(newTip).where(eq(tip.id, newTip.id));
+export async function getTipByUserAndMatch(userId: number, matchId: number): Promise<Tip | undefined> {
+    const [tipResult] = await db.select().from(tip)
+        .where(
+            and(
+                eq(tip.userId, userId),
+                eq(tip.matchId, matchId)
+            )
+        ).limit(1);
+    return tipResult;
 }
 
+export async function deleteTip(userId: number, matchId: number) {
+    await db.delete(tip).where(and(eq(tip.userId, userId), eq(tip.matchId, matchId)));
+}
---
+++ tests/integration/lib/tip.test.ts
@@ -0,0 +1,51 @@
+import {afterEach,  expect, test} from 'vitest'
+import {deleteTip, getTipByUserAndMatch, saveTip} from '../../../src/lib/tip'
+import db from "../../../src/core/db.ts";
+import {and, eq} from "drizzle-orm";
+import {tip} from "../../../db/schemas/schema.ts";
+afterEach(async () => {
+    await deleteTip(1,1);
+})
+
+
+test("test tip", async () => {
+    let result = await getTipByUserAndMatch(1,1);
+
+    expect(result).toBeUndefined();
+
+    await saveTip(1,1,3,2);
+
+    result = await getTipByUserAndMatch(1,1);
+
+    expect(result).toBeDefined();
+    expect(result.userId).toBe(1);
+    expect(result.matchId).toBe(1);
+    expect(result.scoreHome).toBe(3);
+    expect(result.scoreAway).toBe(2);
+
+    await saveTip(1,1,1,3);
+
+    result = await getTipByUserAndMatch(1,1);
+    expect(result).toBeDefined();
+    expect(result.userId).toBe(1);
+    expect(result.matchId).toBe(1);
+    expect(result.scoreHome).toBe(1);
+    expect(result.scoreAway).toBe(3);
+})
+
+test("test update tipdate", async () => {
+
+    let date = new Date();
+    await saveTip(1,1,3,2);
+    let result = await getTipByUserAndMatch(1,1);
+    const currentDate = new Date();
+    let dateOneSecondAgo = new Date(currentDate.getTime() - 1000);
+    result.date = dateOneSecondAgo;
+    await db.update(tip).set(result).where(eq(tip.id, result.id));
+
+    await saveTip(1,1,1,3);
+
+    result = await getTipByUserAndMatch(1,1);
+
+    expect(result.date.getTime()).toBeGreaterThan(dateOneSecondAgo.getTime());
+})
