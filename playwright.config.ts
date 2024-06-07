5c05ccfd990880068fc71f73a65f7d504ab0165b
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-08 00:17:43 +0200

    add playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ dotenv.config({ path: path.resolve('.env.test') });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/acceptance',
+  testDir: './tests_e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -26,15 +26,14 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   webServer: {
-    command: 'bun astro build && bun astro preview',
-    url: 'http://localhost:4321/',
+    command: 'pnpm astro build && pnpm astro preview --port=4322',
+    url: 'http://localhost:4322/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: 'http://localhost:4321/',
+    baseURL: 'http://localhost:4322/',
   },
-
 
   /* Configure projects for major browsers */
   projects: [
@@ -43,79 +42,9 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        storageState: 'playwright/.auth/user.json',
+        //storageState: 'playwright/.auth/user.json',
       },
       dependencies: ['setup'],
     },
-
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        storageState: 'playwright/.auth/user.json',
-      },
-      dependencies: ['setup'],
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-        storageState: 'playwright/.auth/user.json',
-      },
-      dependencies: ['setup'],
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
-
-
-// const config: PlaywrightTestConfig  = {
-//   testDir: './tests/acceptance',
-//   webServer: {
-//     command: 'bun astro build && bun astro preview',
-//     url: 'http://localhost:4321/',
-//     timeout: 120 * 1000,
-//     reuseExistingServer: !process.env.CI,
-//   },
-//   use: {
-//     baseURL: 'http://localhost:4321/',
-//   },
-//   projects: [
-//     {
-//       name: 'chromium',
-//       use: {...devices['Desktop Chrome']},
-//     },
-//     {
-//       name: 'webkit',
-//       use: { ...devices['Desktop Safari'] },
-//     },
-//   ]
-// };
-// export default config;
