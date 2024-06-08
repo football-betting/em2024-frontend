b39dc34574c07ade81ed6c3d3db6098a49c01eb4
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-08 22:01:52 +0200

    test

--- tests/integration/pages/api/auth/login.test.ts
+++ tests/integration/pages/api/auth/login.test.ts
@@ -18,4 +18,27 @@ describe('POST API Route', () => {
         expect(jsonResponse.error).toBeDefined();
         expect(jsonResponse.error).toBe('Ungültige E-Mail');
     });
+
+    it('returns error when password is invalid', async () => {
+        const mockContext = {
+            request: {
+                formData: vi.fn().mockResolvedValue({
+                    get: vi.fn((key) => {
+                        const data = {
+                            email: 'test@test.com',
+                            password: '123'
+                        };
+                        return data[key];
+                    }),
+                })
+            }
+        };
+
+        const response = await POST(mockContext);
+
+        expect(response.status).toBe(400);
+        const jsonResponse = await response.json();
+        expect(jsonResponse.error).toBeDefined();
+        expect(jsonResponse.error).toBe('Ungültiges Passwort');
+    });
 });
