995fa0a9f2e689863b17606b077cb01da4bbc767
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2024-06-08 21:57:46 +0200

    test

--- tests/integration/pages/api/auth/login.test.ts
+++ tests/integration/pages/api/auth/login.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it, vi } from 'vitest';
+import { POST } from '../../../../../src/pages/api/auth/login';
 
 describe('POST API Route', () => {
     it('returns error when email is invalid', async () => {
